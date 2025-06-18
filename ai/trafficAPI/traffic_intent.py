ac29234484bde3ac36eede028e13c43965bcda8c
donghajang213 <alja34@naver.com>
2025-06-18 12:36:40 +0900

    subway intent, entity 수정

--- ai/trafficAPI/traffic_intent.py
+++ ai/trafficAPI/traffic_intent.py
@@ -5,13 +5,15 @@ class FindingWayEntity(BaseModel):
     type: Literal["출발지", "도착지", "교통수단"]
     value: str
 
-class RealtimeArrivalEntity(BaseModel):
+class RealtimeBusEntity(BaseModel):
     type: Literal["정류장", "노선"]
     value: str
 
 class RealtimeSubwayEntity(BaseModel):  # ✅ 새로 추가된 부분
-    type: Literal["지하철역"]
+    type: Literal["지하철역", "방면", "호선", "방향"]
     value: str
 
 class TrafficIntent(BaseModel):
-    intent: Literal["길찾기", "실시간 도착 정보", "실시간 지하철 도착 정보"]
+    intent: Literal["길찾기",
+                    "실시간 도착 정보",
+                    "실시간 지하철 도착 정보"]
