f9213c728d6d1653ce1f1108f6cd567e97ca3b74
chosuna <sunaaa114@gmail.com>
2025-06-20 09:32:02 +0900

    refactor: API 반환 형식 수정

--- ai/fastapi_openapi/test_streamlit.py
+++ ai/fastapi_openapi/test_streamlit.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import requests
+import pandas as pd
 
 st.title("🚇 지하철 도착 정보 챗봇")
 
@@ -23,16 +24,37 @@ if st.button("질문하기") and user_input:
                 data = res.json()
                 api_results = []
                 for result in data["results"]:
-                    for api_result in result["API_results"]:
-                        if type(api_result) == list:
-                            api_results.append('\n\n'.join(api_result))
+                    if result["entity_results"]["intent"] == "미세먼지":
+                        api_result = result["API_results"]
+                        if api_result:
+                            local = api_result["local"]
+                            dust_type = api_result["dust_type"]
+                            dust_value = api_result["dust_value"]
+                            dataTime = api_result["dataTime"]
+                            st.success(f"✅ 미세먼지 API 호출 성공\n\n지역 : {local}\n\n먼지 유형:{dust_type}\n\n농도: {dust_value} ㎍/㎥\n\n측정 시간: {dataTime}")
                         else:
-                            api_results.append(api_result)
-                api_results[0] = api_results[0].replace('\n', '\n\n')
-                api_results = '\n\n'.join(str(x) for x in api_results)
-                st.success(f"💬\n\n{api_results}")
+                            st.error(f"❌ 미세먼지 API 호출 실패: {res.status_code}")
+                    elif result["entity_results"]["intent"] == "날씨":
+                        api_result = result["API_results"]
+                        if api_result:
+                            # st.success(api_result)
+                            df = pd.DataFrame(api_result)
+                            st.success(f"✅ 날씨 API 호출 성공\n\n")
+                            st.title("📊 날씨 예보 결과")
+                            st.dataframe(df, use_container_width=True)
+                    else:
+                        for api_result in result["API_results"]:
+                            if type(api_result) == list:
+                                api_result = '\n\n'.join(api_result).replace('\n', '\n\n')
+                                api_results.append(api_result)
+                            else:
+                                api_results.append(api_result)
+                        api_results = '\n\n\n\n'.join(str(x) for x in api_results)
+                        print(api_results)
+                        api_results = api_results.replace('\n', '\n\n')
+                        st.success(f"💬\n\n{api_results}")
             else:
                 st.error(f"❌ 서버 오류: {res.status_code}")
 
         except Exception as e:
-            st.error(f"🚨 요청 중 오류 발생: {e}")
+            st.error(f"더 자세하게 질문을 입력해주세요.\n\n오류: {e}")
