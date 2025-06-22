19c2568ab05937f31b399bc73b011a5646334bdb
chosuna <sunaaa114@gmail.com>
2025-06-22 18:30:16 +0900

    feat: FastAPI한테 반환값 받으면 질문 답변 DB에 저장 및 DB 테이블 수정

---
+++ backend/src/main/java/com/universe/universe/repository/CategoryRepository.java
@@ -0,0 +1,10 @@
+package com.universe.universe.repository;
+
+import com.universe.universe.entity.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    Optional<Category> findByName(String name);
+}
